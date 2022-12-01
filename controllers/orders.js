f83ded44ebac451c01382c55e55b6fd43aefc332
stephanminges <stephanminges@gmail.com>
2022-11-30 20:33:13 -0500

    fixes

--- controllers/orders.js
+++ controllers/orders.js
@@ -54,7 +54,7 @@ export async function getUserOrders(req, res) {
   try {
     const orders = await Order.find({
       user: mongoose.Types.ObjectId(req.user.id),
-    });
+    }).populate("user");
 
     return res
       .status(200)
